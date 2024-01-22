5ac217600a440386e58363394d9b5dd8296c4f9c
TomiB98 <tomasbaldi@gmail.com>
2024-01-22 11:08:29 -0300

    Unidad 5 - reCaptcha v3

--- unidad5.php
+++ unidad5.php
@@ -6,6 +6,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="estilos.css">
 	<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+	<script>
+		function onSubmit(token) {
+			document.getElementById("demo-form").submit();
+		}
+	</script>
 </head>
 
 <body>
@@ -67,7 +72,9 @@
 				<input class="imputFormCaptcha" type="text" placeholder="Apellido" name="surname" required maxlength="50">
 				<input class="imputFormCaptcha" type="email" placeholder="Email" name="email" required maxlength="50">
 				<textarea class="imputCaptchaText" type="text" placeholder="Consulta" name="cons" required maxlength="255"></textarea>
+
 				<div class="g-recaptcha" data-sitekey="6LfSv1gpAAAAAAgC2RFy6KQMR2UyAVyLiY0EQkFj"></div>
+
 				<div class="submitComentario">
 					<input class="submitUnidad1" type="submit" value="Enviar">
 				</div>
@@ -75,6 +82,15 @@
 
 			<h3>• ReCaptcha v3:</h3>
 
+			<form class="formCaptcha" action="componentes/cargar.php" method="POST">
+				<input class="imputFormCaptcha" type="text" placeholder="Nombre" name="name" required maxlength="30">
+				<input class="imputFormCaptcha" type="text" placeholder="Apellido" name="surname" required maxlength="50">
+				<input class="imputFormCaptcha" type="email" placeholder="Email" name="email" required maxlength="50">
+				<textarea class="imputCaptchaText" type="text" placeholder="Consulta" name="cons" required maxlength="255"></textarea>
+
+				<button class="g-recaptcha" data-sitekey="6LcBxlgpAAAAANQAOi5fIAtxK3mZ6xk0qsTH35nB" data-callback='onSubmit' data-action='submit'>Submit</button>
+
+			</form>
 
 		</section>
 		<aside>
